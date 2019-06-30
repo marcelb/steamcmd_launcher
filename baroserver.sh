98d394657771491c2344f85c224507b5f5f0b1a0
marcelb <adsci81@gmail.com>
2019-06-30 17:19:31 +0200

    serversettings + removal of dead code

--- baroserver.sh
+++ baroserver.sh
@@ -40,28 +40,6 @@ check_server_name()
 	fi
 }
 
-update_and_run()
-{
-	while false
-	do
-		cd ~/$SERVER_NAME
-		~/Steam/steamcmd.sh \
-			+login anonymous \
-			+force_install_dir ~/$SERVER_NAME/Barotrauma \
-			+app_update 1026340 validate \
-			+quit
-		rm -f ~/$SERVER_NAME/Barotrauma/serversettings.xml
-		cp ~/$SERVER_NAME/serversettings.xml ~/$SERVER_NAME/Barotrauma
-		cd ~/$SERVER_NAME/Barotrauma
-		./DedicatedServer
-		cd ~/$SERVER_NAME
-		echo "Press CTRL+C to stop..."
-		sleep 5
-	done
-}
-
-
-
 # PROGRAM START
 
 echo -e "${PINK}Welcome to SteamCMD Runner 0.1${NC}"
