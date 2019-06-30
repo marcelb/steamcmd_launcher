948a9668967753c943e02e631dc206065990b29a
marcelb <adsci81@gmail.com>
2019-06-30 15:55:29 +0200

    remove validate, better crontab

--- launcher.sh
+++ launcher.sh
@@ -8,13 +8,16 @@ STEAMCMD=~/Steam/steamcmd.sh
 
 echo "Running Server with parameters: $ROOT_DIR, $SERVER_NAME, $APP_ID"
 
+#For fixing server files:
+#  +app_update $APP_ID validate 
+
 while true
 do
 	cd $ROOT_DIR/$SERVER_NAME
 	$STEAMCMD \
 		+login anonymous \
 		+force_install_dir $ROOT_DIR/$SERVER_NAME/Barotrauma \
-		+app_update $APP_ID validate \
+		+app_update $APP_ID \
 		+quit
 	rm -f $ROOT_DIR/$SERVER_NAME/Barotrauma/serversettings.xml
 	cp $ROOT_DIR/${SERVER_NAME}_serversettings.xml $ROOT_DIR/$SERVER_NAME/Barotrauma/serversettings.xml
--- updateAll.sh
+++ updateAll.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Crontab helper for the nightly update
+# 0 5 * * * ~/BarotraumaServer/updateAll.sh
+cd ~/BarotraumaServer
 ~/BarotraumaServer/baroserver.sh stop Baro1
 sleep 5
 ~/BarotraumaServer/baroserver.sh start Baro1
